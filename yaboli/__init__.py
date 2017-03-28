3b3ce99625db0b465be9b7d0766b0c73bb73bf86
Joscha <brummer@migejolise.de>
2017-03-28 07:39:11 +0000

    Add logging and log formats

--- yaboli/__init__.py
+++ yaboli/__init__.py
@@ -1,1 +1,9 @@
+import logging
+logging.basicConfig(
+	level=logging.DEBUG,
+	format="[{levelname}] in {threadName: <13} <{name}>: {message}",
+	style="{"
+)
+
 from .basic_types import Message, SessionView
+from .callbacks import Callbacks
