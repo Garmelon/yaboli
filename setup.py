c728ff331a762777ce9e5d00e62a859f4e1423dc
jeremyredhead <jeremyredhead@users.noreply.github.com>
2018-02-19 20:50:28 +0000

    Add a SetupTools setup.py file
    This will making installing & using yaboli easier

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='yaboli',
+        version='1.0', 
+        description='Yet Another BOt LIbrary for euphoria.io',
+        author='Garmelon',
+        url='https://github.com/Garmelon/yaboli',
+        packages=['yaboli'],
+        install_requires=['websockets']
+)
