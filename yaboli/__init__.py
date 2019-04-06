f6f7cc5aa6113d9b6cc6d4a0844ddf26b6ab41a8
Joscha <joscha@plugh.de>
2019-04-06 17:17:43 +0000

    Clean up

--- yaboli/__init__.py
+++ yaboli/__init__.py
@@ -1,21 +1,19 @@
 from typing import List
 
-__all__: List[str] = []
-
 from .client import *
-__all__ += client.__all__
-
+from .connection import *
+from .events import *
 from .exceptions import *
-__all__ += client.__all__
-
 from .message import *
-__all__ += exceptions.__all__
-
 from .room import *
-__all__ += message.__all__
-
-__all__ += room.__all__
 from .user import *
-
-__all__ += user.__all__
 from .util import *
+
+__all__: List[str] = []
+__all__ += client.__all__
+__all__ += connection.__all__
+__all__ += events.__all__
+__all__ += exceptions.__all__
+__all__ += message.__all__
+__all__ += room.__all__
+__all__ += user.__all__
